b2518f99d9b2c56e728126edaf44deb33416bdd0
Stephen Leitnick <sleitnick@gmail.com>
2019-12-19 13:16:40 -0500

    Update dependencies & test structure

---
+++ src/test/suite/index.ts
@@ -0,0 +1,31 @@
+import * as path from "path";
+import * as Mocha from "mocha";
+import * as glob from "glob";
+
+export function run(): Promise<void> {
+    const mocha = new Mocha({
+        ui: "tdd"
+    });
+    mocha.useColors(true);
+    const testsRoot = path.resolve(__dirname, "..");
+    return new Promise((resolve, reject): void => {
+        glob("**/**.test.js", {cwd: testsRoot}, (err, files): void => {
+            if (err) {
+                return reject(err);
+            }
+            files.forEach((f): Mocha => mocha.addFile(path.resolve(testsRoot, f)));
+            try {
+                mocha.run((failures): void => {
+                    if (failures > 0) {
+                        reject(new Error(`${failures} tests failed`));
+                    } else {
+                        resolve();
+                    }
+                });
+            } catch(err) {
+                console.error(err);
+                reject(err);
+            }
+        });
+    });
+}
